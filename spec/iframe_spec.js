f3e54b30109411e7bde31917b23bb07b00b11648
Michael Darmousseh <michael.darmousseh@gmail.com>
2013-03-11 12:23:37 -0700

    specs pass now

--- spec/iframe_spec.js
+++ spec/iframe_spec.js
@@ -2,27 +2,25 @@ describe("Iframe js", function() {
   var $DOM;
   beforeEach(function() {
     $DOM = $("#jasmine_content");
-    var form = '<form id="form"><input id="form-input" name="form-input" type="text"></form>';
+    var form = '<form id="form" method="GET" action="examples/response.html"><input id="form-input" name="form-input" type="text"></form>';
     $DOM.html("");
     $DOM.append(form);
     expect($DOM.html()).toEqual(form)
   });
 
-  var responseFixture = 'Success<script type="text/javascript">function() {console.info("about to post message");window.postMessage("success :D","localhost");}</script>';
-
-  afterEach(function() {
-  })
   it("should submit the form in the iframe", function() {
-    var $callback = jasmine.createSpy();
+    var called = false;
+    var $callback = function(response){
+      expect(response).toEqual("success!!!")
+      called = true;
+    };
     $DOM.find("#form").iframeSubmit($callback);
-    waits(100);
-    $DOM.find("#form").submit();
-//    waits(100);
-    //Pretend the iframe was submitted
-
-    $DOM.find("iframe").contents().find("html").html(responseFixture);
-    waits(200);
-    expect($callback).toHaveBeenCalledWith("Success!!!")
+    runs(function() {
+      $DOM.find("#form").submit();
+    })
+    waitsFor(function() {
+      return called = true;
+    })
   });
 
 });
--- src/iframe.js
+++ src/iframe.js
@@ -1,7 +1,6 @@
 jQuery.fn.iframeSubmit = function(callback) {
   var self = this;
   self.submit(function(e) {
-    console.info("Submit", e)
     e.preventDefault();
     var iframe = $("<iframe id='iframejs-frame' style='display: none;'></iframe>");
     var form = self.parent().html();
@@ -11,22 +10,16 @@ jQuery.fn.iframeSubmit = function(callback) {
     self.find("input").each(function(index, input) {
       $('#iframejs-frame').contents().find('#' + $(input).attr('id')).val($(input).val());
     });
-    $('#iframejs-frame').contents().find('form').attr("action", 'examples/response.html');
-    $('#iframejs-frame').contents().find('form').attr("method", 'GET');
+    $('#iframejs-frame').contents().find('form').attr("action", self.attr('action'));
+    $('#iframejs-frame').contents().find('form').attr("method", self.attr('method'));
 
-
-//    $('#iframejs-frame').contents().find('form').attr("action", 'https://core.spreedly.com/v1/payment_methods');
-//    $('#iframejs-frame').contents().find('form').attr("method", 'POST');
-//    $('#iframejs-frame').contents().find('form').append('<input name="redirect_url" type="hidden" value="http://local.milyoni.net:3000/response" />');
     $('#iframejs-frame').contents().find('form').submit();
 
-//    callback("Success!!!");
-
     window.addEventListener('message', function(e) {
-      console.info("Callback :D", e);
-      callback(e.data)
+      callback(e.data);
     }, false);
     return false;
   });
 };
 
+//    $('#iframejs-frame').contents().find('form').append('<input name="redirect_url" type="hidden" value="http://local.milyoni.net:3000/response" />');
